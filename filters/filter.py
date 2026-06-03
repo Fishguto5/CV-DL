0aee3acd6dadb714954dcc44b3db25cf2d379e5c
Gustavo Fernandes <gutof.carvalho@gmail.com>
2026-06-03 20:13:46 -0300

    uploding Gans and CNN notebooks

---
+++ filters/filter.py
@@ -0,0 +1,10 @@
+from PIL import Image, ImageFilter
+import numpy as np
+import os
+import cv2 as cv
+
+# INPUT_DIR = "assets"
+# OUTPUT_DIR = "assets_output"
+# image = Image.open(os.path.join(INPUT_DIR,'hector2.jpeg'))
+# filtered = image.filter(ImageFilter.FIND_EDGES)
+# filtered.show()
